d15f39180ea491696a6783392b6e15675e684664
Vox <voxx@blaze.holdings>
2024-05-11 01:33:32 +0200

    Create addons exemple

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,15 +11,21 @@ shared_scripts {
     "src/utils/events.lua",
     "src/utils/main.lua",
     "src/modules/main.lua",
+    -- Addons
+    "addons/**/shared/*.lua",
 }
 
 client_scripts {
     "src/utils/**/client/*.lua",
     "src/modules/**/client/*.lua",
+    -- Addons
+    "addons/**/client/*.lua",
 }
 
 server_scripts {
     "src/modules/**/server/*.lua",
     "src/utils/**/server/*.lua",
     "src/modules/**/server/object/*.lua",
+    -- Addons
+    "addons/**/server/*.lua",
 }
