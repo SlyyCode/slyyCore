62323159e527110c33a0aee9a8b99a41483dd435
Vox <voxx@blaze.holdings>
2024-05-11 20:33:18 +0200

    Add colors server config (rgba)

--- configuration/global.lua
+++ configuration/global.lua
@@ -2,3 +2,9 @@ Config = {}
 Config.Language = "fr" -- Check that the language exists, if it doesn't you can create it
 Config.DebugMod = true -- Enable debug ?
 Config.LocalFile = "resources/slyyCore" -- The or the file is from the resources folder
+Config.ServerColor = {
+    R = 91,
+    G = 157,
+    B = 255,
+    A = 255
+}
