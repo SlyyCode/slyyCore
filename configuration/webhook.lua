d2e37bfef2cba110304ca44f4635e057e0c19db7
root <root@ip-172-26-8-158.eu-west-3.compute.internal>
2024-05-19 23:47:05 +0000

    Create function for inject webhook

--- configuration/webhook.lua
+++ configuration/webhook.lua
@@ -1,2 +1,6 @@
 Webhook = {}
 Webhook["main"] = "https://discord.com/api/webhooks/1241724285349920859/bmgcnXHbl6kOFmmGuIu64j9zkKE6XM1VQdLEa8nHylAtWar0TyxJS9thfkROc9gUtNdf"
+
+slyyCore.injectWebhook = function(id, value)
+    Webhook[id] = value
+end
